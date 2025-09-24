272eb6828b9a6e360fbf03e6d8f15e85ac907ff0
plima94 <psantoslima.dev@gmail.com>
2025-09-24 15:43:10 +0100

    initial commit

---
+++ src/test/java/com/plimadev/virtus/VirtusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plimadev.virtus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
