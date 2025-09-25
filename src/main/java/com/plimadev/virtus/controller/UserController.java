ceff9dc6c6a950e4c9d284bcc160449d51da66f4
plima94 <psantoslima.dev@gmail.com>
2025-09-25 14:50:08 +0100

    created UserController class and some endpoints

--- src/main/java/com/plimadev/virtus/controller/UserController.java
+++ src/main/java/com/plimadev/virtus/controller/UserController.java
@@ -1,14 +1,44 @@
 package com.plimadev.virtus.controller;
 
+import com.plimadev.virtus.dto.RegisterUserRequest;
+import com.plimadev.virtus.dto.UserProfileResponse;
+import com.plimadev.virtus.model.Character;
+import com.plimadev.virtus.service.CharacterService;
 import com.plimadev.virtus.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
 public class UserController {
 
     private final UserService userService;
+    private final CharacterService characterService;
+
+
+    public UserController(UserService userService, CharacterService characterService) {
+        this.userService = userService;
+        this.characterService = characterService;
+
+    }
+
+    @PostMapping("/users/register")
+    public ResponseEntity<UserProfileResponse> registerUser(@RequestBody RegisterUserRequest request) {
+        return ResponseEntity.status(201).body(userService.registerUser(request));
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserProfileResponse> getUserProfile(@PathVariable Long id){
+        return ResponseEntity.ok(userService.getUserProfile((id)));
+    }
+
+    @PatchMapping("/characters/{id}/name")
+    public ResponseEntity<Character> updateCharacterName(
+            @PathVariable Long id,
+            @RequestBody String newName
+    ) {
+        return ResponseEntity.ok(characterService.updateCharacterName(id, newName));
+    }
 
 
 }
